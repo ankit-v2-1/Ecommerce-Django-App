cea2705f2d6ce42974fea758c663da2878d70d1c
Ankit raj <ankitjaiswal7890@gmail.com>
2022-04-11 10:46:05 +0530

    refactor: refactor upload path

--- store/models/product.py
+++ store/models/product.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=500, default='', null=True, blank=True)
-    image = models.ImageField(upload_to='uploads/')
+    image = models.ImageField(upload_to='')
     stock = models.BooleanField(default=False)
 
     def __str__(self):
