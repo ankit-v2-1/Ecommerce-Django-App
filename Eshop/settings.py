e2e0025c5b5cbfdc8b251d61ac0601073afcadd1
Ankit raj <ankitjaiswal7890@gmail.com>
2022-04-27 22:58:40 +0530

    fix: add allowed domains name

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'demo-key-only-for-testing'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','kdfresh.in','www.kdfresh.in']
 
 
 # Application definition
